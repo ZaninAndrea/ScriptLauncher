be29fde0c09046fe61e1e5f1215286f1a1706ee1
ZaninAndrea <99.zanin@gmail.com>
2017-10-15 22:28:30 +0200

    prettier

--- public/electron.js
+++ public/electron.js
@@ -1,104 +1,98 @@
-const {
-  app,
-  BrowserWindow,
-  ipcMain,
-  globalShortcut,
-  shell
-} = require("electron");
-const path = require("path");
-const isDev = require("electron-is-dev");
-const { autoUpdater } = require("electron-updater");
-const Telegraf = require("telegraf");
-const secrets = require("./secret.js");
-const child_process = require("child_process");
-const authorizedUsers = [];
-const bot = new Telegraf(secrets.token);
-let mainWindow;
-let telegram;
+const {app, BrowserWindow, ipcMain, globalShortcut, shell} = require("electron")
+const path = require("path")
+const isDev = require("electron-is-dev")
+const {autoUpdater} = require("electron-updater")
+const Telegraf = require("telegraf")
+const secrets = require("./secret.js")
+const child_process = require("child_process")
+const authorizedUsers = []
+const bot = new Telegraf(secrets.token)
+let mainWindow
+let telegram
 
 // start command runs authorization request throguht the react app
 bot.command("start", ctx => {
-  telegram = ctx.telegram;
-  if (authorizedUsers.indexOf(ctx.from.id) !== -1) {
-    return reply("you are already authorized");
-  } else {
-    mainWindow.webContents.send("authorizationRequest", ctx.from);
-  }
-});
-bot.startPolling();
+    telegram = ctx.telegram
+    if (authorizedUsers.indexOf(ctx.from.id) !== -1) {
+        return reply("you are already authorized")
+    } else {
+        mainWindow.webContents.send("authorizationRequest", ctx.from)
+    }
+})
+bot.startPolling()
 
 ipcMain.on("authorizeUser", (event, user) => {
-  authorizedUsers.push(user);
-  telegram.sendMessage(user, "You've been authorized!");
-});
+    authorizedUsers.push(user)
+    telegram.sendMessage(user, "You've been authorized!")
+})
 
 ipcMain.on("unauthorizeUser", (event, user) => {
-  telegram.sendMessage(user, "Your request has been rejected");
-});
+    telegram.sendMessage(user, "Your request has been rejected")
+})
 
 function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 900,
-    height: 50,
-    alwaysOnTop: true,
-    skipTaskbar: true,
-    frame: false,
-    resizable: false
-  });
-  mainWindow.loadURL(
-    isDev
-      ? "http://localhost:3000"
-      : `file://${path.join(__dirname, "../build/index.html")}`
-  ); // load the react app
-  mainWindow.on("closed", () => (mainWindow = null));
-  mainWindow.on("blur", () => mainWindow.hide());
-  const pos = mainWindow.getPosition();
-  mainWindow.setPosition(pos[0], 150);
+    mainWindow = new BrowserWindow({
+        width: 900,
+        height: 50,
+        alwaysOnTop: true,
+        skipTaskbar: true,
+        frame: false,
+        resizable: false,
+    })
+    mainWindow.loadURL(
+        isDev
+            ? "http://localhost:3000"
+            : `file://${path.join(__dirname, "../build/index.html")}`
+    ) // load the react app
+    mainWindow.on("closed", () => (mainWindow = null))
+    mainWindow.on("blur", () => mainWindow.hide())
+    const pos = mainWindow.getPosition()
+    mainWindow.setPosition(pos[0], 150)
 
-  // intercept link opening
-  mainWindow.webContents.on("new-window", function(event, url) {
-    event.preventDefault();
-    shell.openExternal(url);
-  });
+    // intercept link opening
+    mainWindow.webContents.on("new-window", function(event, url) {
+        event.preventDefault()
+        shell.openExternal(url)
+    })
 
-  mainWindow.hide();
+    mainWindow.hide()
 }
 
 // when the app is loaded create a BrowserWindow and check for updates
 app.on("ready", function() {
-  createWindow();
-  bot.command("shutdown", ({ from, reply }) => {
-    if (authorizedUsers.indexOf(ctx.from.id) !== -1) {
-      reply("Shutting down");
-      child_process.spawn("shutdown /s /t 30", { shell: true });
-    } else {
-      reply("You are not authorized, run the command /start first");
-    }
-  });
-  if (!isDev) autoUpdater.checkForUpdates();
+    createWindow()
+    bot.command("shutdown", ({from, reply}) => {
+        if (authorizedUsers.indexOf(ctx.from.id) !== -1) {
+            reply("Shutting down")
+            child_process.spawn("shutdown /s /t 30", {shell: true})
+        } else {
+            reply("You are not authorized, run the command /start first")
+        }
+    })
+    if (!isDev) autoUpdater.checkForUpdates()
 
-  // Register a 'CommandOrControl+X' shortcut listener.
-  const ret = globalShortcut.register("Ctrl+Q", () => {
-    console.log("clicked");
-    if (mainWindow.isVisible()) {
-      mainWindow.hide();
-    } else {
-      mainWindow.show();
-    }
-  });
-});
+    // Register a 'CommandOrControl+X' shortcut listener.
+    const ret = globalShortcut.register("Ctrl+Q", () => {
+        console.log("clicked")
+        if (mainWindow.isVisible()) {
+            mainWindow.hide()
+        } else {
+            mainWindow.show()
+        }
+    })
+})
 
 // when the update has been downloaded and is ready to be installed, notify the BrowserWindow
 autoUpdater.on("update-downloaded", info => {
-  mainWindow.webContents.send("updateReady");
-});
+    mainWindow.webContents.send("updateReady")
+})
 
 // when receiving a quitAndInstall signal, quit and install the new version ;)
 ipcMain.on("quitAndInstall", (event, arg) => {
-  autoUpdater.quitAndInstall();
-});
+    autoUpdater.quitAndInstall()
+})
 
 app.on("will-quit", () => {
-  // Unregister all shortcuts.
-  globalShortcut.unregisterAll();
-});
+    // Unregister all shortcuts.
+    globalShortcut.unregisterAll()
+})
--- src/App.js
+++ src/App.js
@@ -1,87 +1,87 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, {Component} from "react"
+import "./App.css"
 
-const electron = window.require("electron"); // little trick to import electron in react
-const ipcRenderer = electron.ipcRenderer;
+const electron = window.require("electron") // little trick to import electron in react
+const ipcRenderer = electron.ipcRenderer
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleKeyPress = this.handleKeyPress.bind(this);
+    constructor(props) {
+        super(props)
+        this.handleChange = this.handleChange.bind(this)
+        this.handleKeyPress = this.handleKeyPress.bind(this)
 
-    this.state = {
-      updateReady: false,
-      authorizationRequest: false,
-      from: {},
-      value: ""
-    };
+        this.state = {
+            updateReady: false,
+            authorizationRequest: false,
+            from: {},
+            value: "",
+        }
 
-    ipcRenderer.on("updateReady", (event, text) => {
-      this.setState({ updateReady: true });
-    });
-    ipcRenderer.on("authorizationRequest", (event, from) => {
-      this.setState({ authorizationRequest: true, from });
-    });
-  }
-
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
-
-  handleKeyPress(event) {
-    if (event.key === "Enter") {
-      if (this.state.value.startsWith("wolfram ")) {
-        window.open(
-          "https://www.wolframalpha.com/input/?i=" +
-            this.state.value.substring("wolfram ".length)
-        );
-      } else if (this.state.value.startsWith("open ")) {
-        window.open(this.state.value.substring("open ".length));
-      }
-      this.setState({ value: "" });
+        ipcRenderer.on("updateReady", (event, text) => {
+            this.setState({updateReady: true})
+        })
+        ipcRenderer.on("authorizationRequest", (event, from) => {
+            this.setState({authorizationRequest: true, from})
+        })
     }
-  }
-  // render() {
-  //     return <div>
-  //       Foobar
-  //
-  //       <button onClick={()=>ipcRenderer.send('quitAndInstall')}>{this.state.updateReady ? "click to update" : "no updates ready"}</button>
-  //       {this.state.authorizationRequest ?
-  //           <div>Telegram user {this.state.from.username} is asking authorization to run command
-  //               <button onClick={()=>{
-  //                   ipcRenderer.send("authorizeUser", this.state.from.id)
-  //                   this.setState({authorizationRequest: false})
-  //               }}>Allow</button>
-  //               <button onClick={()=>{
-  //                   ipcRenderer.send("unauthorizeUser", this.state.from.id)
-  //                   this.setState({authorizationRequest: false})
-  //               }}>Dismiss</button>
-  //           </div>
-  //           : ""}
-  //     </div>
-  // }
 
-  render() {
-    return (
-      <div className="App">
-        <input
-          style={{
-            width: "896px",
-            height: "46px",
-            fontSize: "46px"
-          }}
-          autoFocus
-          className="inputLine"
-          placeholder="enter command"
-          type="text"
-          value={this.state.value}
-          onChange={this.handleChange}
-          onKeyPress={this.handleKeyPress}
-        />
-      </div>
-    );
-  }
+    handleChange(event) {
+        this.setState({value: event.target.value})
+    }
+
+    handleKeyPress(event) {
+        if (event.key === "Enter") {
+            if (this.state.value.startsWith("wolfram ")) {
+                window.open(
+                    "https://www.wolframalpha.com/input/?i=" +
+                        this.state.value.substring("wolfram ".length)
+                )
+            } else if (this.state.value.startsWith("open ")) {
+                window.open(this.state.value.substring("open ".length))
+            }
+            this.setState({value: ""})
+        }
+    }
+    // render() {
+    //     return <div>
+    //       Foobar
+    //
+    //       <button onClick={()=>ipcRenderer.send('quitAndInstall')}>{this.state.updateReady ? "click to update" : "no updates ready"}</button>
+    //       {this.state.authorizationRequest ?
+    //           <div>Telegram user {this.state.from.username} is asking authorization to run command
+    //               <button onClick={()=>{
+    //                   ipcRenderer.send("authorizeUser", this.state.from.id)
+    //                   this.setState({authorizationRequest: false})
+    //               }}>Allow</button>
+    //               <button onClick={()=>{
+    //                   ipcRenderer.send("unauthorizeUser", this.state.from.id)
+    //                   this.setState({authorizationRequest: false})
+    //               }}>Dismiss</button>
+    //           </div>
+    //           : ""}
+    //     </div>
+    // }
+
+    render() {
+        return (
+            <div className="App">
+                <input
+                    style={{
+                        width: "896px",
+                        height: "46px",
+                        fontSize: "46px",
+                    }}
+                    autoFocus
+                    className="inputLine"
+                    placeholder="enter command"
+                    type="text"
+                    value={this.state.value}
+                    onChange={this.handleChange}
+                    onKeyPress={this.handleKeyPress}
+                />
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
